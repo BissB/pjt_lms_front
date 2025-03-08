748ab5e53f31079ffed0b31c5f62602f55829b0c
Matthew <shin.joonchul@gmail.com>
2025-03-09 00:26:33 +0900

    app.js modification && navbar folder creation && installation of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import './App.css';
 
 import Login from './login/Login';
-import  Course_manage  from './course_overview';
+import Navbar from './navbar/Navbar';
+import { Course_manage } from './course_overview';
 import { Course_check } from './course_details';
 import { Member } from './member_list/';
-import Navbar from './Navbar';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 function App() {
+  const location = useLocation(); // 현재 경로를 가져옴
+  const excludedRoutes = ['/']; // Navbar를 숨길 경로 목록
+
   return (
     <div className="App">
       <div className='main'>
-        <Navbar />
-        {/* <Login /> */}
-        {/* <Course_manage/> */}
-        {/* <Course_check/> */}
-        {/* <Member /> */}
+        {!excludedRoutes.includes(location.pathname) && <Navbar />}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/course_overview" element={<Course_manage />} />
+          <Route path="/course_detail" element={<Course_check />} />
+          <Route path="/member_list" element={<Member />} />
+        </Routes>
       </div>
     </div>
   );
 }
 
-export default App;
+const RootApp = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default RootApp;
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import RootApp from './App';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
-    <App />
+    <RootApp />
 );
