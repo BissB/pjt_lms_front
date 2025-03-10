3e0a29082e28e3206dc1bdd41add2f849f61ef66
Matthew <shin.joonchul@gmail.com>
2025-03-10 14:55:54 +0900

    회원등록 구현테스트

--- src/member_list/Member_registration.js
+++ src/member_list/Member_registration.js
@@ -2,26 +2,94 @@ import React, { useState } from 'react';
 import styles from './Member_registration.module.css';
 
 const Member_registration = ({ onClose }) => {
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [registerForm, setRegisterForm] = useState({
+    memberType: '',
+    memberId: '',
+    memberPassword: '',
+    memberName: '',
+    memberPhone: '',
+    memberCourse: '',
+    memberFile: '',
+  });
 
-  const handlePhoneNumberChange = (event) => {
-    const input = event.target.value;
-    const cleanedNumber = input.replace(/\D+/g, '');
-    const formattedPhoneNumber = formatPhoneNumber(cleanedNumber);
-    setPhoneNumber(formattedPhoneNumber);
+  // 입력값 변경 핸들러
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setRegisterForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
   };
 
-  const formatPhoneNumber = (number) => {
-    if (number.length <= 3) {
-      return number;
-    } else if (number.length <= 6) {
-      return `${number.substring(0, 3)} - ${number.substring(3)}`;
-    } else {
-      return `${number.substring(0, 3)} - ${number.substring(3, 7)} - ${number.substring(7)}`;
-    }
+  // 역할 변경 핸들러
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setRegisterForm((prevForm) => ({
+      ...prevForm,
+      memberFile: file,
+    }));
   };
+
+  // 파일 변경 핸들러
+  const handleRoleChange = (event) => {
+    setRegisterForm((prevForm) => ({
+      ...prevForm,
+      memberType: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(registerForm);
+
+    const formData = new FormData();
+    formData.append('memberType', registerForm.memberType);
+    formData.append('memberId', registerForm.memberId);
+    formData.append('memberPassword', registerForm.memberPassword);
+    formData.append('memberName', registerForm.memberName);
+    formData.append('memberPhone', registerForm.memberPhone);
+    formData.append('memberCourse', registerForm.memberCourse);
+    formData.append('memberFile', registerForm.memberFile);
+
+    // const [phoneNumber, setPhoneNumber] = useState('');
+
+    // const handlePhoneNumberChange = (event) => {
+    //   const input = event.target.value;
+    //   const cleanedNumber = input.replace(/\D+/g, '');
+    //   const formattedPhoneNumber = formatPhoneNumber(cleanedNumber);
+    //   setPhoneNumber(formattedPhoneNumber);
+    // };
+
+    // const formatPhoneNumber = (number) => {
+    //   if (number.length <= 3) {
+    //     return number;
+    //   } else if (number.length <= 6) {
+    //     return `${number.substring(0, 3)} - ${number.substring(3)}`;
+    //   } else {
+    //     return `${number.substring(0, 3)} - ${number.substring(3, 7)} - ${number.substring(7)}`;
+    //   }
+    // };
+
+    // 백엔드로 데이터전송
+    fetch('/project/register', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+    console.log(formData.get('memberType'));
+    console.log(formData.get('memberId'));
+    console.log(formData.get('memberPassword'));
+    console.log(formData.get('memberName'));
+    console.log(formData.get('memberPhone'));
+    console.log(formData.get('memberCourse'));
+    console.log(formData.get('memberFile'));
+  }
 
   const handleCancelClick = () => {
+    
     onClose();
   };
 
@@ -30,45 +98,79 @@ const Member_registration = ({ onClose }) => {
       <div className={styles.background}>
         <div className={styles.container}>
           <h2>회원 등록</h2>
-          <div className={styles.form}>
-            <div className={styles.type}>
-              <div>
-                <input type="radio" id="student" name="type" value="student" />
-                <label for="student">훈련생</label>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.form}>
+              <div className={styles.radio}>
+
+                <div>
+                  <input 
+                  type="radio" 
+                  id="admin" 
+                  name="type" 
+                  value="1" 
+                  checked={registerForm.memberType === '1'}
+                  onChange={handleRoleChange}
+                  />
+                  <label for="admin">관리자</label>
+                </div>
+
+                <div>
+                  <input 
+                    type="radio" 
+                    id="instructor" 
+                    name="type" 
+                    value="2"
+                    checked={registerForm.memberType === '2'}
+                    onChange={handleRoleChange}
+                    />
+                  <label for="instructor">강사</label>
+                </div>
+
+
+                <div>
+                  <input 
+                    type="radio" 
+                    id="student" 
+                    name="type" 
+                    value="3"
+                    checked={registerForm.memberType === '3'}
+                    onChange={handleRoleChange}
+                    />
+                  <label for="student">훈련생</label>
+                </div>
+
               </div>
 
-              <div>
-                <input type="radio" id="instructor" name="type" value="instructor" />
-                <label for="instructor">강사</label>
+              {/* 입력 필드 */}
+              <input 
+              type="text" 
+              className={styles.textbox} 
+              placeholder="아이디" 
+              name="memberId"
+              value={registerForm.memberId}
+              onChange={handleInputChange}
+              />
+              <input type="password" className={styles.textbox} placeholder="비밀번호" />
+              <input type="text" className={styles.textbox} placeholder="이름" />
+              <input
+                type="tel"
+                className={styles.textbox}
+                placeholder="전화번호"
+                maxLength={10}
+              // value={phoneNumber}
+              // onChange={handlePhoneNumberChange}
+              // maxLength={17}
+              />
+              <input type="text" className={styles.textbox} placeholder="신청과정" />
+              <div className={styles.photo}>
+                <input type="file" accept="image/*" />
               </div>
-
-              <div>
-                <input type="radio" id="admin" name="type" value="admin" />
-                <label for="admin">관리자</label>
+              <div className={styles.btns}>
+                <button type="submit" className={styles.submit} >등록</button>
+                <button type="button" className={styles.cancel} onClick={handleCancelClick} >취소</button>
               </div>
             </div>
-
-            <input type="text" className={styles.textbox} placeholder="아이디" />
-            <input type="password" className={styles.textbox} placeholder="비밀번호" />
-            <input type="text" className={styles.textbox} placeholder="이름" />
-            <input
-              type="tel"
-              className={styles.textbox}
-              placeholder="전화번호"
-              maxLength={10}
-            // value={phoneNumber}
-            // onChange={handlePhoneNumberChange}
-            // maxLength={17}
-            />
-            <input type="text" className={styles.textbox} placeholder="신청과정" />
-            <div className={styles.photo}>
-              <input type="file" accept="image/*" />
-            </div>
-            <div className={styles.btns}>
-              <button type="submit" className={styles.submit}>등록</button>
-              <button type="button" className={styles.cancel} onClick={handleCancelClick} >취소</button>
-            </div>
-          </div>
+          </form>
         </div>
       </div>
     </>
