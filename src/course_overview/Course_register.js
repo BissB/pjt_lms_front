35613abd6a4e0cb88552c92c7410e3203a46e332
Yenju <pplopy@naver.com>
2025-03-13 14:15:54 +0900

    manage css update

--- src/course_overview/Course_register.js
+++ src/course_overview/Course_register.js
@@ -10,52 +10,52 @@ const Course_register = () => {
         navigate("/course_overview");
     }
 
-    const [formData, setFormData] = useState({
-            type: "",
-            name: "",
-            instructor: "",
-            startDate: "",
-            endDate: "",
-            capacity: "",
-            detail: "",
-            file:""
-    });
+    // const [formData, setFormData] = useState({
+    //         type: "",
+    //         name: "",
+    //         instructor: "",
+    //         startDate: "",
+    //         endDate: "",
+    //         capacity: "",
+    //         detail: "",
+    //         file:""
+    // });
 
-    const handleRegisterConfirm = async () => {
+//     const handleRegisterConfirm = async () => {
 
-        if (!courseName || !instructorName || !startDate || !endDate || !capacity) {
-            alert("모든 필수 정보를 입력하세요.");
-            return;
-        }
+//         if (!courseName || !instructorName || !startDate || !endDate || !capacity) {
+//             alert("모든 필수 정보를 입력하세요.");
+//             return;
+//         }
         
-        const data = new FormData();
-        data.append("courseType", courseType);
-        data.append("courseName", courseName);
-        data.append("instructorName", instructorName);
-        data.append("startDate", startDate);
-        data.append("endDate", endDate);
-        data.append("capacity", capacity);
-        data.append("description", description);
-//        if (file) {
-//           formData.append("file", file);
-//        }
+//         const data = new FormData();
+//         data.append("courseType", courseType);
+//         data.append("courseName", courseName);
+//         data.append("instructorName", instructorName);
+//         data.append("startDate", startDate);
+//         data.append("endDate", endDate);
+//         data.append("capacity", capacity);
+//         data.append("description", description);
+// //        if (file) {
+// //           formData.append("file", file);
+// //        }
 
-        try {
-            const response = await fetch(`https://localhost:443/course`, {
-            method: "PUT",
-            });
+//         try {
+//             const response = await fetch(`https://localhost:443/course`, {
+//             method: "PUT",
+//             });
 
-            if (response.ok) {
-                alert("등록되었습니다.");
-            } else {
-                alert("등록 실패: ");
-            }
-        } catch (error) {
-            console.error("등록 오류:", error);
-        }
+//             if (response.ok) {
+//                 alert("등록되었습니다.");
+//             } else {
+//                 alert("등록 실패: ");
+//             }
+//         } catch (error) {
+//             console.error("등록 오류:", error);
+//         }
 
-        onClose();
-    };
+//         onClose();
+//     };
 
     return(
         <div className={styles.main}>
@@ -94,10 +94,10 @@ const Course_register = () => {
                     <input className={styles.file} type='file'/>
                     <p className={styles.contentboxheadline}>&lt; 내용 &gt;</p>
                     <textarea className={styles.contentbox}/>
-                    <div className={styles.buttonbox}>
+                    {/* <div className={styles.buttonbox}>
                         <button onClick={handleRegisterConfirm} className={styles.savebutton}>저장</button>
                         <button onClick={onClose} className={styles.cancelbutton}>취소</button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
