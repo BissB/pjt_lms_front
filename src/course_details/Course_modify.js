a17319481e95c1fa12ee430994a5c2fe8a01af9f
taeyoung9601 <rlaxoduddlqslek@naver.com>
2025-03-16 16:10:50 +0900

    과정 수정에서 status 값 수정하도록 변경

--- src/course_details/Course_modify.js
+++ src/course_details/Course_modify.js
@@ -23,6 +23,7 @@ const Course_modify = () => {
         currCount: "",
         capacity: "",
         detail: "",
+        status:"",
     });
 
     const [modifyFile, setModifyFile] = useState(null);
@@ -40,6 +41,7 @@ const Course_modify = () => {
                    capacity: data.capacity || "",
                    currCount: data.currCount || "",
                    detail: data.detail || "",
+                   status: data.status || "",
                });
            })
            .catch((error) => console.error("데이터 불러오기 실패:", error));
@@ -73,6 +75,7 @@ const Course_modify = () => {
             formData.append("capacity", modifyFormData.capacity);
             formData.append("currCount", modifyFormData.currCount);
             formData.append("detail", modifyFormData.detail);
+            formData.append("status", modifyFormData.status);
 
             if (modifyFile) {
                 formData.append("upfiles", modifyFile);
@@ -106,15 +109,27 @@ const Course_modify = () => {
             <div className={styles.container}>
                 <div className={styles.leftbox}>
                     <div className={styles.titletext}>과정 정보 관리</div>
-                    <div className={styles.dropdown}>구분 
-                        <select value={modifyFormData.type} className={styles.select} name= "type"
-                         onChange={(e) => handleChange("type", e.target.value)}>
-                            <option value="">선택</option>
-                            <option value="1">NCS</option>
-                            <option value="2">KDT</option>
-                            <option value="3">산대특</option>
-                            <option value="4">미정</option>
-                        </select>
+                    <div className={styles.selectcontainer}>
+                        <div className={styles.dropdown}>구분 
+                            <select value={modifyFormData.type} className={styles.select} name= "type"
+                            onChange={(e) => handleChange("type", e.target.value)}>
+                                <option value="">선택</option>
+                                <option value="1">NCS</option>
+                                <option value="2">KDT</option>
+                                <option value="3">산대특</option>
+                                <option value="4">미정</option>
+                            </select>
+                        </div>
+                        <div className={styles.dropdown}>상태 
+                            <select value={modifyFormData.status} className={styles.select} name= "status"
+                            onChange={(e) => handleChange("status", e.target.value)}>
+                                <option value="">선택</option>
+                                <option value="1">진행예정</option>
+                                <option value="2">진행중</option>
+                                <option value="3">폐지</option>
+                                <option value="4">진행종료</option>
+                            </select>
+                        </div>
                     </div>
                     <div className={styles.inputcontainer}>과정명
                         <input className={styles.inputbox} name="name"  value={modifyFormData.name} onChange={(e) => handleChange("name", e.target.value)}></input> 
